53167ca58c59ae13210bcd7c9b26b648f236b761
Andrew Molloy <andy@andymolloy.net>
2012-06-01 09:41:13 -0400

    Start work on song collection class

---
+++ LilJukeBox/LilJukeBox/ASMSongCollection.h
@@ -0,0 +1,17 @@
+//
+//  ASMSongCollection.h
+//  LilJukeBox
+//
+//  Created by Andrew Molloy on 5/31/12.
+//  Copyright (c) 2012 Andy Molloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASMSongCollection : NSObject {
+    NSMutableArray* songIdentifiers;
+}
+
+
+
+@end
---
+++ LilJukeBox/LilJukeBox/ASMSongCollection.m
@@ -0,0 +1,13 @@
+//
+//  ASMSongCollection.m
+//  LilJukeBox
+//
+//  Created by Andrew Molloy on 5/31/12.
+//  Copyright (c) 2012 Andy Molloy. All rights reserved.
+//
+
+#import "ASMSongCollection.h"
+
+@implementation ASMSongCollection
+
+@end
