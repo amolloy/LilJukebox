311e61725bb9a5259dc820980fdf82d48273ac04
Andy Molloy <asmolloy@me.com>
2012-10-03 16:04:57 -0400

    Dynamically create song buttons based on how much room there is for buttons and how many songs are selected. Highlight buttons while they are selected.

--- LilJukeBox/LilJukeBox/ASMMainViewController.m
+++ LilJukeBox/LilJukeBox/ASMMainViewController.m
@@ -14,39 +14,84 @@
 
 NSString* kHideConfigUserDefaultsKey = @"HideConfig";
 
+enum {
+	kButtonSize = 64,
+	kMinButtonSpacing = 10,
+};
+
 @interface ASMMainViewController ()
 
 @property (retain, nonatomic) IBOutlet UIButton *flipViewButton;
 @property (retain, nonatomic) NSArray* songButtons;
 @property (retain, nonatomic) IBOutlet UIView *containerView;
 @property (retain, nonatomic) IBOutlet UIView *infoContainerView;
+@property (retain, nonatomic) IBOutlet UIView *buttonContainerView;
 
-- (IBAction)songButtonPressed:(UIButton *)sender;
+- (void)songButtonPressed:(UIButton *)sender;
+- (void)songButtonChangedState:(UIButton *)sender;
 
 @end
 
 @implementation ASMMainViewController
 
-@synthesize flipViewButton = _flipViewButton;
+- (void)songButtonChangedState:(UIButton*)sender
+{
+	if (sender.isHighlighted)
+	{
+		sender.backgroundColor = [ASMSongCollection hightlightColorForSongIndex:sender.tag - 1];
+		sender.layer.borderColor = [ASMSongCollection colorForSongIndex:sender.tag - 1].CGColor;
+	}
+	else
+	{
+		sender.backgroundColor = [ASMSongCollection colorForSongIndex:sender.tag - 1];
+		sender.layer.borderColor = [ASMSongCollection hightlightColorForSongIndex:sender.tag - 1].CGColor;
+	}
+}
 
-@synthesize flipsidePopoverController = _flipsidePopoverController;
+- (void)setupSongButtons
+{
+	for (UIButton* button in self.songButtons)
+	{
+		[button removeFromSuperview];
+	}
+
+	NSInteger maxButtons = (NSInteger)floorf(self.buttonContainerView.frame.size.width / (CGFloat)(kButtonSize + kMinButtonSpacing));
+	NSInteger numButtons = [[ASMSongCollection sharedSongCollection] songCount];
+	if (numButtons > maxButtons)
+	{
+		numButtons = maxButtons;
+	}
+	NSInteger buttonXInc = (NSInteger)((self.buttonContainerView.frame.size.width - kButtonSize) / (numButtons - 1));
+	
+	NSMutableArray* buttons = [NSMutableArray arrayWithCapacity:numButtons];
+	
+	for (NSInteger i = 0; i < numButtons; ++i)
+	{
+		UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+		button.frame = CGRectMake(i * buttonXInc, 0, kButtonSize, kButtonSize);
+		button.tag = i + 1;
+		[self songButtonChangedState:button];
+		
+		button.layer.cornerRadius = button.frame.size.width / 2;
+		button.layer.borderWidth = 3;
+		
+		[button addTarget:self
+				   action:@selector(songButtonPressed:)
+		 forControlEvents:UIControlEventTouchUpInside];
+		[button addTarget:self
+				   action:@selector(songButtonChangedState:)
+		 forControlEvents:UIControlEventAllTouchEvents];
+		
+		[buttons addObject:button];
+		[self.buttonContainerView addSubview:button];
+	}
+	
+	self.songButtons = [[buttons copy] autorelease];
+}
 
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
-	
-	NSMutableArray* buttons = [NSMutableArray arrayWithCapacity:[ASMSongCollection maxSongs]];
-	
-	for (NSUInteger i = 0; i < [ASMSongCollection maxSongs]; ++i)
-	{
-		UIView* songButton = [self.view viewWithTag:i + 1]; // Start tags at 1
-		[buttons addObject:songButton];
-		
-		songButton.backgroundColor = [ASMSongCollection colorForSongIndex:i];
-		songButton.layer.cornerRadius = songButton.frame.size.width / 2;
-	}
-	
-	self.songButtons = [buttons copy];
 	
 	self.containerView.layer.cornerRadius = 10;
 	self.containerView.layer.borderColor = [UIColor darkGrayColor].CGColor;
@@ -59,34 +104,16 @@ NSString* kHideConfigUserDefaultsKey = @"HideConfig";
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    NSArray* songs = [[ASMSongCollection sharedSongCollection] songs];
-    NSUInteger songCount = [songs count];
-    
-    for (NSUInteger i = 0; i < [ASMSongCollection maxSongs]; ++i)
-    {
-        UIButton* button = [self.songButtons objectAtIndex:i];
-
-        BOOL songAvailable = (i < songCount);
-        
-        button.enabled = songAvailable;
-		
-		if (songAvailable)
-		{
-			button.backgroundColor = [ASMSongCollection colorForSongIndex:i];
-		}
-		else
-		{
-			button.backgroundColor = [UIColor darkGrayColor];
-		}
-    }
-    
     self.flipViewButton.hidden = [[NSUserDefaults standardUserDefaults] boolForKey:kHideConfigUserDefaultsKey];
+	
+	[self setupSongButtons];
 }
 
 - (void)viewDidUnload
 {
 	[self setContainerView:nil];
 	[self setInfoContainerView:nil];
+	[self setButtonContainerView:nil];
     [super viewDidUnload];
     self.songButtons = nil;
 }
@@ -94,11 +121,6 @@ NSString* kHideConfigUserDefaultsKey = @"HideConfig";
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
 	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
-}
-
-- (BOOL)shouldAutorotate
-{
-	return NO;
 }
 
 #pragma mark - Flipside View Controller
@@ -121,6 +143,7 @@ NSString* kHideConfigUserDefaultsKey = @"HideConfig";
     [self.songButtons release];
 	[_containerView release];
 	[_infoContainerView release];
+	[_buttonContainerView release];
     [super dealloc];
 }
 
@@ -161,7 +184,7 @@ NSString* kHideConfigUserDefaultsKey = @"HideConfig";
     }
 }
 
-- (IBAction)songButtonPressed:(UIButton *)sender
+- (void)songButtonPressed:(UIButton *)sender
 {
     NSUInteger tag = sender.tag;
     NSUInteger songIndex = tag - 1;
@@ -183,6 +206,14 @@ NSString* kHideConfigUserDefaultsKey = @"HideConfig";
     MPMusicPlayerController* appMusicPlayer = [MPMusicPlayerController applicationMusicPlayer];
     [appMusicPlayer setQueueWithItemCollection:collection];
     [appMusicPlayer play];
+	
+	for (UIButton* button in self.songButtons)
+	{
+		if (button != sender)
+		{
+			[self songButtonChangedState:button];
+		}
+	}
 }
 
 @end
